bc9371cf0a8cf90455d6a5534dd01d4306addd51
javiscripto <javier.mecker94@gmail.com>
2023-09-11 22:16:54 -0300

    incorporo lógica en route carts

--- src/routes/carts.route.js
+++ src/routes/carts.route.js
@@ -2,7 +2,48 @@ const express= require("express");
 const route=express.Router();
 
 
+const carts=[];
+let Id=0;
 
 
+//crear nuevo carrito
+route.post("/api/carts",(req,res)=>{
+    
+    const cart={id:Id++,products:[]}
+    carts.push(cart);
+    res.json({message:`se ha creado nuevo carrito id`})
+})
+
+
+//get all carts
+route.get("/api/carts",(req,res)=>{
+    res.json(carts)
+})
+
+//get cart by id
+route.get("/api/carts/:cid", (req, res)=>{
+    const idCart= parseInt(req.params.cid) ;
+    const cart = carts.find((c)=>c.id===idCart);
+    cart?res.json(cart.products):res.status(400).json({message:`not found`})
+
+})
+
+//add product cart
+route.post("/api/carts/:cid/product/:pid", (req, res)=>{
+    const idCart= parseInt(req.params.cid) ;
+    const idProd=parseInt(req.params.pid);
+
+    const indexCart= carts.indexOf(carts.find((c)=>c.id===idCart))
+    if(carts[indexCart].products.includes(idProd)){
+        carts[indexCart].products.quantity+=1;
+        res.json({message:`se ha agregado el nuevo producto`})
+    }else{
+        const product={id:idProd,quantity:1};
+        carts[indexCart].products.push(product)
+        res.json({message:`se agregado otro producto`});
+    };
+   
+
+})
 
 module.exports=route;
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,19 +1,80 @@
 const express=require("express");
 const route=express.Router();
 
-const products=[];
+
+
+
+let products=[
+    {
+        "title": "manzana",
+        "description": "manzana dulce",
+        "code": "a01",
+        "price": "1565",
+        "status": "true",
+        "stock": "26541",
+        "cat": "frutas",
+        "id": 0
+    },
+    {
+        "title": "pera",
+        "description": "pera de agua",
+        "code": "a02",
+        "price": "1565",
+        "status": "true",
+        "stock": "98798",
+        "cat": "frutas",
+        "id": 1
+    },  {
+        "title": "naranja",
+        "description": "naranja norteña",
+        "code": "a02",
+        "price": "156",
+        "status": "true",
+        "stock": "261",
+        "cat": "citricas",
+        "id": 2
+    }
+];
+
+
 
 let Id=0;
-
+//all products
 route.get("/products",(requ,res)=>{
     res.json(products)
 });
 
-
-route.post("/products",(req,res)=>{
-    const newProduct=req.body;
-    products.push({...newProduct,id:Id++})
-    res.json({message:"sussessful"})
+//product by id
+route.get("/products/:pid",(requ,res)=>{
+    const id = Number(requ.params.pid);
+    let product= products.find((prod)=>prod.id==id);
+    product?res.json({product}):res.status(400).send(`product not found`)
 })
 
+//add new product
+route.post("/products",(req,res)=>{
+    const newProduct=req.body;
+    products.push({...newProduct,id:Id++});
+    res.json({message:` ${newProduct.title} added to products list`})
+});
+
+
+//update product
+route.put("/porducts/:pid", (requ,res)=>{
+    const productId= parseInt(requ.params.pid) ; 
+    const updateFields= requ.body
+    let index= products.indexOf(products.find((prod)=>prod.id===productId));
+    if (index!=-1)res.json({message:`producto actualizado`})
+
+
+});
+
+
+// delete product by
+route.delete("/products/:pid", (requ,res)=>{
+    const id= parseInt(requ.params.pid);
+    let index= products.indexOf(products.find((prod)=>prod.id===id));
+    products.splice(index,1)
+    res.json({message:`se ha eliminado el producto`})
+})
 module.exports= route;
